8619aa807ddd1967231284491cf7fd4261ae8538
Karlatemp <karlatemp@vip.qq.com>
2020-04-01 19:20:31 +0800

    Update Ver

--- MiraiBootstrap/src/cn/mcres/karlatemp/mirai/Bootstrap.java
+++ MiraiBootstrap/src/cn/mcres/karlatemp/mirai/Bootstrap.java
@@ -59,7 +59,7 @@ public class Bootstrap {
         return LoginData.getLoginPasswd();
     }
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static String getVersion() {
         return VERSION;
